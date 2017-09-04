6bd94484a86f2b04e89941d4a3bceac5efc30254
Timothé Larivière <timothe.lariviere@outlook.com>
2017-09-04 11:28:32 +0200

    v0.1.4

--- WoodenMoose.FunctionalExtensions/Rop/AsyncFuncExtensions.cs
+++ WoodenMoose.FunctionalExtensions/Rop/AsyncFuncExtensions.cs
@@ -30,11 +30,34 @@ namespace WoodenMoose.FunctionalExtensions.Rop
             };
         }
 
+        #region ThenAsync
+
         /// <summary>
         /// Chains another function after the current one
+        /// void -> T; T -> TResult1
         /// </summary>
         /// <typeparam name="T">Input data type of the incoming response</typeparam>
         /// <typeparam name="TResult1">Output data type of the outcoming response</typeparam>
+        /// <param name="function1">Current function</param>
+        /// <param name="function2">Function to chain after</param>
+        /// <returns>A new function representing the two chained functions</returns>
+        public static Func<Task<Response<TResult1>>> ThenAsync<T, TResult1>(this Func<Response<T>> function1, Func<T, Response<TResult1>> function2)
+        {
+            return async () =>
+            {
+                var asyncFunc1 = MakeAsync(function1);
+                var asyncFunc2 = MakeAsync(function2);
+                var bindedFunc2 = Bind(asyncFunc2);
+                return await bindedFunc2(await asyncFunc1());
+            };
+        }
+
+        /// <summary>
+        /// Chains another function after the current one
+        /// void -> T; T -> Task<TResult1>
+        /// </summary>
+        /// <typeparam name="T">Input data type of the incoming response</typeparam>
+        /// <typeparam name="TResult1">Output data type of the intermediate outcoming response</typeparam>
         /// <param name="function1">Current function</param>
         /// <param name="function2">Function to chain after</param>
         /// <returns>A new function representing the two chained functions</returns>
@@ -50,25 +73,26 @@ namespace WoodenMoose.FunctionalExtensions.Rop
 
         /// <summary>
         /// Chains another function after the current one
+        /// void -> Task<T>; T -> TResult1
         /// </summary>
         /// <typeparam name="T">Input data type of the incoming response</typeparam>
-        /// <typeparam name="TResult1">Output data type of the intermediate outcoming response</typeparam>
-        /// <typeparam name="TResult2">Output data type of the outcoming response</typeparam>
+        /// <typeparam name="TResult1">Output data type of the outcoming response</typeparam>
         /// <param name="function1">Current function</param>
         /// <param name="function2">Function to chain after</param>
         /// <returns>A new function representing the two chained functions</returns>
-        public static Func<Response<T>, Task<Response<TResult2>>> ThenAsync<T, TResult1, TResult2>(this Func<T, Response<TResult1>> function1, Func<TResult1, Task<Response<TResult2>>> function2)
+        public static Func<Task<Response<TResult1>>> ThenAsync<T, TResult1>(this Func<Task<Response<T>>> function1, Func<T, Response<TResult1>> function2)
         {
-            return async value =>
+            return async () =>
             {
-                var asyncFunc1 = MakeAsync(function1);
-                var bindedFunc2 = Bind(function2);
-                return await bindedFunc2(await asyncFunc1(value.Data));
+                var asyncFunc2 = MakeAsync(function2);
+                var bindedFunc2 = Bind(asyncFunc2);
+                return await bindedFunc2(await function1());
             };
         }
 
         /// <summary>
         /// Chains another function after the current one
+        /// void -> Task<T>; T -> Task<TResult1>
         /// </summary>
         /// <typeparam name="T">Input data type of the incoming response</typeparam>
         /// <typeparam name="TResult1">Output data type of the outcoming response</typeparam>
@@ -86,21 +110,81 @@ namespace WoodenMoose.FunctionalExtensions.Rop
 
         /// <summary>
         /// Chains another function after the current one
+        /// T -> TResult1; TResult1 -> TResult2
         /// </summary>
         /// <typeparam name="T">Input data type of the incoming response</typeparam>
         /// <typeparam name="TResult1">Output data type of the outcoming response</typeparam>
         /// <param name="function1">Current function</param>
         /// <param name="function2">Function to chain after</param>
         /// <returns>A new function representing the two chained functions</returns>
-        public static Func<Task<Response<TResult1>>> ThenAsync<T, TResult1>(this Func<Task<Response<T>>> function1, Func<T, Response<TResult1>> function2)
+        public static Func<T, Task<Response<TResult2>>> ThenAsync<T, TResult1, TResult2>(this Func<T, Response<TResult1>> function1, Func<TResult1, Response<TResult2>> function2)
         {
-            return async () =>
+            return async value =>
             {
-                var asyncedFunc2 = MakeAsync(function2);
-                var bindedFunc2 = Bind(asyncedFunc2);
-                return await bindedFunc2(await function1());
+                var asyncFunc1 = MakeAsync(function1);
+                var asyncFunc2 = MakeAsync(function2);
+                var bindedFunc2 = Bind(asyncFunc2);
+                return await bindedFunc2(await asyncFunc1(value));
             };
         }
+
+        /// <summary>
+        /// Chains another function after the current one
+        /// T -> TResult1; TResult1 -> Task<TResult2>
+        /// </summary>
+        /// <typeparam name="T">Input data type of the incoming response</typeparam>
+        /// <typeparam name="TResult1">Output data type of the intermediate outcoming response</typeparam>
+        /// <param name="function1">Current function</param>
+        /// <param name="function2">Function to chain after</param>
+        /// <returns>A new function representing the two chained functions</returns>
+        public static Func<T, Task<Response<TResult2>>> ThenAsync<T, TResult1, TResult2>(this Func<T, Response<TResult1>> function1, Func<TResult1, Task<Response<TResult2>>> function2)
+        {
+            return async value =>
+            {
+                var asyncFunc1 = MakeAsync(function1);
+                var bindedFunc2 = Bind(function2);
+                return await bindedFunc2(await asyncFunc1(value));
+            };
+        }
+
+        /// <summary>
+        /// Chains another function after the current one
+        /// T -> Task<TResult1>; TResult1 -> TResult2
+        /// </summary>
+        /// <typeparam name="T">Input data type of the incoming response</typeparam>
+        /// <typeparam name="TResult1">Output data type of the outcoming response</typeparam>
+        /// <param name="function1">Current function</param>
+        /// <param name="function2">Function to chain after</param>
+        /// <returns>A new function representing the two chained functions</returns>
+        public static Func<T, Task<Response<TResult2>>> ThenAsync<T, TResult1, TResult2>(this Func<T, Task<Response<TResult1>>> function1, Func<TResult1, Response<TResult2>> function2)
+        {
+            return async value =>
+            {
+                var asyncFunc2 = MakeAsync(function2);
+                var bindedFunc2 = Bind(asyncFunc2);
+                return await bindedFunc2(await function1(value));
+            };
+        }
+
+        /// <summary>
+        /// Chains another function after the current one
+        /// T -> Task<TResult1>; TResult1 -> Task<TResult2>
+        /// </summary>
+        /// <typeparam name="T">Input data type of the incoming response</typeparam>
+        /// <typeparam name="TResult1">Output data type of the outcoming response</typeparam>
+        /// <param name="function1">Current function</param>
+        /// <param name="function2">Function to chain after</param>
+        /// <returns>A new function representing the two chained functions</returns>
+        public static Func<T, Task<Response<TResult2>>> ThenAsync<T, TResult1, TResult2>(this Func<T, Task<Response<TResult1>>> function1, Func<TResult1, Task<Response<TResult2>>> function2)
+        {
+            return async value =>
+            {
+                var bindedFunc2 = Bind(function2);
+                return await bindedFunc2(await function1(value));
+            };
+        }
+
+        #endregion
 
         /// <summary>
         /// Transforms a synchronous Func into an asynchronous one
--- WoodenMoose.FunctionalExtensions/Rop/FuncHelper.cs
+++ WoodenMoose.FunctionalExtensions/Rop/FuncHelper.cs
@@ -36,5 +36,13 @@ namespace WoodenMoose.FunctionalExtensions.Rop
         /// <param name="func">The method to convert</param>
         /// <returns>Func representation of the function</returns>
         public static Func<T, Task<Response<TResult>>> AsFunc<T, TResult>(Func<T, Task<Response<TResult>>> func) => func;
+
+        /// <summary>
+        /// Converts an asynchronous function to its Func representation
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func">The method to convert</param>
+        /// <returns>Func representation of the function</returns>
+        public static Func<T, Task<Response<TResult[]>>> AsFunc<T, TResult>(Func<T, Task<Response<TResult[]>>> func) => func;
     }
 }
